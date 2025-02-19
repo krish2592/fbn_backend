bd60c3f115d2a5ea1715914697efac1537f4ae5b
krish2592 <sachink9225s@gmail.com>
2025-02-19 19:29:23 +0530

    added save payment

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -1,69 +1,39 @@
-import CryptoJS from 'crypto-js';
-import axios from 'axios';
+import Payment from '../models/paymentModel.js';
 
 // Payment endpoint to handle payment creation
-export const initiatePayment = async (req, res) => {
+export const savePayment = async (req, res) => {
     try {
-        console.log("initiatePayment", req.body)
-        //const baseurl="https://infytechai.com";
-        const baseurl = "http://localhost:3000";
-        const uat_URL = process.env.UAT_URL || "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay";
-        // const prod_URL = process.env.PROD_URL || "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay";
+       const {userId, razorpayPaymentId, contestName, 
+        amount,quantity, pricePerQuantity, email, number, status, description} = req.body;
 
-        const reqData = req.body; // Receiving request from Android
-        const dt = new Date();
-        const txnid = 'TX' + dt.getFullYear() + (dt.getMonth() + 1) + dt.getDate() + Date.now();
-        // Temporary need to generate from user
-        const muid = 'MUID' + Math.floor(Math.random(3) * 1000) + Date.now();
-        // Payment request payload
-        const data = {
-            "merchantId": process.env.MERCHANT_ID || "PGTESTPAYUAT",
-            "merchantTransactionId": txnid,
-            "merchantUserId": muid,
-            "amount": reqData.amount * 100, // Convert amount to paise
-            "redirectUrl": `http://localhost:3000/status/${txnid}`,
-            "redirectMode": "POST",
-            "mobileNumber": reqData.number || null,
-            "paymentInstrument": {
-                "type": "PAY_PAGE" // To use PhonePe's pay page interface
-            }
-        };
-       
-        console.log(data);
-
-        // Encode payload to Base64
-        const payload = Buffer.from(JSON.stringify(data)).toString('base64');
-
-        // Generate checksum using SHA256
-        const saltKey = process.env.SALT_KEY || "099eb0cd-02cf-4e2a-8aca-3e6c6aff0399";
-        const saltIndex = 1;
-        const sha256 = CryptoJS.SHA256(payload + "/pg/v1/pay" + saltKey)
-        const checksum = sha256 + '###' + saltIndex;
-        console.log({print:checksum})
-        // Send request to PhonePe API
-        const options = {
-            method: 'POST',
-            url: uat_URL,
-            headers: {
-                accept: 'application/json',
-                'Content-Type': 'application/json',
-                'X-VERIFY': checksum
-            },
-            data: {
-                request: payload
-            }
-        };
-
-        // Request PhonePe for payment URL
-        const response = await axios(options);
-
-        if (response.data.success) {
-            res.status(200).send({
-                paymentUrl: response.data.data.instrumentResponse.redirectInfo.url
-            });
-        } else {
-            res.status(400).send({ message: "Failed to create payment" });
+        const payment = new Payment({
+            userId,
+            razorpayPaymentId,
+            contestName,
+            amount,
+            quantity,
+            pricePerQuantity,
+            email,
+            phone: number,
+            status,
+            description
         }
+        )
+       
+        console.log({payment:payment})
+
+        const resp = await payment.save(payment);
+
+        console.log(resp);
+
+        res.send({
+            success: "success", 
+            message:"Payment saved successfully", 
+            data: {
+            id: resp._id,
+            amount: resp.amount
+        }
+    })
 
     } catch (error) {
         res.status(500).send({
@@ -75,33 +45,13 @@ export const initiatePayment = async (req, res) => {
 
 
 export const getPaymentStatus = async (req, res) => {
-    const merchantTransactionId = res.req.body.transactionId
-    const merchantId = res.req.body.merchantId
-    const saltKey = process.env.SALT_KEY || "099eb0cd-02cf-4e2a-8aca-3e6c6aff0399"
-    const keyIndex = 1;
-    const string = `/pg/v1/status/${merchantId}/${merchantTransactionId}` + saltKey;
-    const sha256 = CryptoJS.SHA256(string)
-    const checksum = sha256 + "###" + keyIndex;
-    //const uat_URL = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/";
-    console.log({print:checksum})
-    const options = {
-        method: 'GET',
-        url: prod_URL+`/pg/v1/` + `status/${merchantId}/${merchantTransactionId}`,
-        headers: {
-            accept: 'application/json',
-            'Content-Type': 'application/json',
-            'X-VERIFY': checksum,
-            'X-MERCHANT-ID': `${merchantId}`
-        }
-    };
+    try {
 
-    const response = await axios.request(options);
-    console.log(response.data);
-    if(response.data.success && response.data.state === "COMPLETED") {
-        const url = `${baseurl}/success`
-        return res.redirect(url);
-    } else {
-        const url = `${baseurl}/failure`
-        return res.redirect(url);
+
+    } catch(error) {
+        res.status(500).send({
+            message: error.message,
+            success: false
+        });
     }
 }
---
+++ src/models/paymentModel.js
@@ -0,0 +1,61 @@
+// models/User.js
+import mongoose from "mongoose";
+
+const paymentSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: String,
+        },
+        razorpayPaymentId:{
+            type: String,
+            required: true,
+            unique: true,
+        },
+        contestName: {
+            type: String,
+            required: true,
+        },
+        amount: {
+            type: String,
+            required: true,
+        },
+        quantity: {
+            type: String,
+            required: true,
+        },
+        pricePerQuantity: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String
+        },
+        phone: {
+            type: String
+        },
+        status: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        isDeleted: {
+            type: Boolean,
+            default: false
+        },
+        deletedAt: {
+            type: Boolean
+        }
+    },
+    { timestamps: true }
+);
+
+const User = mongoose.model("Payment", paymentSchema);
+
+export default User;
